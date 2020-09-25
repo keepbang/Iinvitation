2546948138b65f79de720ed59a5f7c8c8e969e77
keepbang <rlqud1125@naver.com>
2020-09-25 11:32:12 +0900

    update photo

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import '../css/Photo.scss'
+import {Photo_List} from './PhotoImg';
 
 function Photo() {
     return(
         <div className="photo_page">
-            사진첩
+            {
+                Photo_List.map((v) => {
+                    return <a key={v.id} href={v.src}>
+                            <img className="photo_img" alt="pictures" src={v.src}/>
+                        </a>
+                })
+            }
         </div>
     )
 }
---
+++ src/components/PhotoImg.jsx
@@ -0,0 +1,48 @@
+import photo1 from '../photo/photo1.jpg';
+import photo2 from '../photo/photo2.jpg';
+import photo3 from '../photo/photo3.jpg';
+import photo4 from '../photo/photo4.jpg';
+import photo5 from '../photo/photo5.jpg';
+import photo6 from '../photo/photo6.jpg';
+import photo7 from '../photo/photo7.jpg';
+import photo8 from '../photo/photo8.jpg';
+import photo9 from '../photo/photo9.jpg';
+
+export const Photo_List = [
+    {
+        id: 1,
+        src: photo1
+    },
+    {
+        id: 2,
+        src: photo2
+    },
+    {
+        id: 3,
+        src: photo3
+    },
+    {
+        id: 4,
+        src: photo4
+    },
+    {
+        id: 5,
+        src: photo5
+    },
+    {
+        id: 6,
+        src: photo6
+    },
+    {
+        id: 7,
+        src: photo7
+    },
+    {
+        id: 8,
+        src: photo8
+    },
+    {
+        id: 9,
+        src: photo9
+    },
+]
